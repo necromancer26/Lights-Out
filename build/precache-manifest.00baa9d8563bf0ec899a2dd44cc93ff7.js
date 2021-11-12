b8e31a8f7cdad89f9b086893875149be562112c0
khalil06 <khalil.chettaoui06@gmail.com>
2021-11-12 16:21:18 +0100

    build

---
+++ build/precache-manifest.00baa9d8563bf0ec899a2dd44cc93ff7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b398c54c4125e75739f4",
+    "url": "/static/css/main.7e93f79a.chunk.css"
+  },
+  {
+    "revision": "b398c54c4125e75739f4",
+    "url": "/static/js/main.b398c54c.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a144f5f42c1c389f8c6c93dd9c5527d",
+    "url": "/index.html"
+  }
+];
